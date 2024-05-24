662c4ed425a39897464adaa97039662de4919353
MarcAwaad <89856850+MarcAwaad@users.noreply.github.com>
2024-05-24 15:40:27 -0400

    Added unittests for new-user form

--- src/app/new-user/new-user.component.spec.ts
+++ src/app/new-user/new-user.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { NewUserComponent } from './new-user.component';
 
 describe('NewUserComponent', () => {
@@ -8,7 +8,10 @@ describe('NewUserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NewUserComponent]
+      imports: [
+        NewUserComponent,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
     
@@ -19,5 +22,38 @@ describe('NewUserComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have an invalid form when empty', () => {
+    expect(component.userForm.valid).toBeFalsy();
+  });
+
+  it('should validate the form', () => {
+    component.userForm.controls['name'].setValue("Name");
+    component.userForm.controls['job'].setValue("Worker");
+    expect(component.userForm.valid).toBeTruthy();
+  });
+
+  it('should have an error if user name or job is not present', () => {
+    let name = component.userForm.controls['name'];
+    let job = component.userForm.controls['job'];
+    expect(name.valid).toBeFalsy();
+    expect(job.valid).toBeFalsy();
+
+    let nameErrors = name.errors || {};
+    let jobErrors = job.errors || {};
+    expect(nameErrors['required']).toBeTruthy();
+    expect(jobErrors['required']).toBeTruthy();
+  });
+
+  it('should call onSubmit when form is valid', () => {
+    spyOn(component, 'onSubmit');
+    component.userForm.controls['name'].setValue("Name");
+    component.userForm.controls['job'].setValue("Worker");
+
+    const button = fixture.nativeElement.querySelector('button');
+    button.click();
+
+    expect(component.onSubmit).toHaveBeenCalled();
   });
 });
--- src/app/user-details/user-details.component.spec.ts
+++ src/app/user-details/user-details.component.spec.ts
@@ -1,24 +1,25 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { UserHandlingComponent } from '../userhandling/userhandling.component';
-// import { UserDetailsComponent } from './user-details.component';
+/* import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UserHandlingComponent } from '../userhandling/userhandling.component';
+import { UserDetailsComponent } from './user-details.component';
 
-// describe('UserDetailsComponent', () => {
-//   let component: UserDetailsComponent;
-//   let fixture: ComponentFixture<UserDetailsComponent>;
+describe('UserDetailsComponent', () => {
+  let component: UserDetailsComponent;
+  let fixture: ComponentFixture<UserDetailsComponent>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [UserDetailsComponent],
-//       providers: [UserHandlingComponent],
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UserDetailsComponent],
+      providers: [UserHandlingComponent],
+    })
+    .compileComponents();
     
-//     fixture = TestBed.createComponent(UserDetailsComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(UserDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+ */
